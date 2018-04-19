b08f4a1402aa25543d097d16e902cbc3cba878cf
GIYEON KWON <brucek0206@gmail.com>
2018-04-19 17:35:17 +0900

    Change Folder Structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 /* JS는 이 파일내에서만 작업해주세요. */
 
 // 수정하지 마세요.
-import CHAT_DATA from './lib/chatHistory.json';
+import CHAT_DATA from '../lib/chatHistory.json';
 
 // `CHAT_DATA`라는 변수에 채팅 히스토리가 담겨있습니다. 이 데이터를 이용하시면 됩니다.
 console.log(CHAT_DATA);
